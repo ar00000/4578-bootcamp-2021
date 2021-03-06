3f9b557f74c90c069c1cbf08ac77eeb9e5893d45
Abhisheka Raman <abhishekaraman@gmail.com>
2021-03-06 14:15:28 +0530

    Q3:Implement GET http request for Employee to get list of employees.

--- src/main/java/com/bootcamp/RestFulWebServicePart1/employee/EmployeeDaoService.java
+++ src/main/java/com/bootcamp/RestFulWebServicePart1/employee/EmployeeDaoService.java
@@ -1,12 +1,21 @@
 package com.bootcamp.RestFulWebServicePart1.employee;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class EmployeeDaoService {
 
     public static List<Employee> employeeList = new ArrayList<>();
     public static int employeeCounter = 0;
+
+    static {
+        employeeList.add(new Employee(1,"John",30));
+        employeeList.add(new Employee(2,"Dena",25));
+        employeeList.add(new Employee(3,"Draco",28));
+    }
 
     public List<Employee> showAllEmployee(){
         return employeeList;
