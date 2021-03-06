fec3c9888765531d0d983643f5169fd66a054533
Abhisheka Raman <abhishekaraman@gmail.com>
2021-03-06 14:20:58 +0530

    Q4:Implement GET http request using path variable to get one employee.

--- src/main/java/com/bootcamp/RestFulWebServicePart1/employee/EmployeeResource.java
+++ src/main/java/com/bootcamp/RestFulWebServicePart1/employee/EmployeeResource.java
@@ -2,6 +2,7 @@ package com.bootcamp.RestFulWebServicePart1.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +16,10 @@ public class EmployeeResource {
     @GetMapping(path = "/employees")
     public List<Employee> showEmployees(){
         return service.showAllEmployee();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getOneEmployee(@PathVariable int id){
+        return service.findEmployee(id);
     }
 }
